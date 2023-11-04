1775350a58a73015a6b2817cbd0ac4f34cf8cc5c
ChristarM <146460668+ChristarM@users.noreply.github.com>
2023-11-04 09:13:57 +0100

    Create eco2.py

---
+++ eco2.py
@@ -0,0 +1,8 @@
+import random
+
+
+lista=["Fai la raccolta differenziata","Non lasciare i rifiuti per la strada","Usa mezzi di trasporto sostenibili","Non usare auto o mezzi di trasporto quando puoi anche andare a piedi","Usa una borraccia riutilizzabile","Usa meno plastica","Non sprecare energia","Non sprecare acqua","Non sprecare il cibo","Portati il cibo da casa per non comprare quelli negli involucri di plastica"]
+
+def scelta(sc):
+    pass
+    
